5f7a2209d23ca4ac61dd20658fba063299bbcac0
iproleplay <tigersplayrole@gmail.com>
2026-02-25 16:58:17 -0300

    dashboard completo profissional

---
+++ static/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "financeiro-v1";
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        "/",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
